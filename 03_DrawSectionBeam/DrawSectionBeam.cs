0e9a9dba8140224cc75982e841c3a9bb353eb7fc
MrDinhHoa <dinhhoanghoa@gmail.com>
2023-08-21 17:29:36 +0700

    Update

--- 03_DrawSectionBeam/DrawSectionBeam.cs
+++ 03_DrawSectionBeam/DrawSectionBeam.cs
@@ -37,7 +37,8 @@ namespace _03_DrawSectionBeam
             PromptPointResult ppR = ed.GetPoint(ppo);
             Point3d insertPoint3D = ppR.Value;
             Point2d point2D = new Point2d(insertPoint3D.X,insertPoint3D.Y);
-            
+            double cover = 25;
+            double fillet = 15;
             using (Transaction Tx = acCurDb.TransactionManager.StartTransaction())
             {
                 try
@@ -47,6 +48,7 @@ namespace _03_DrawSectionBeam
                     int lastrow = lastData.Row;
                     for (int i = firstrow; i <= 50; i++)
                     {
+                        //Lấy dữ liệu từ file excel
                         string nameBeam = (string)(activeSheet.Cells[i, 2] as Range).Value;
                         string localtion = (string)(activeSheet.Cells[i, 3] as Range).Value;
                         var width = (activeSheet.Cells[i, 6] as Range).Value;
@@ -54,26 +56,70 @@ namespace _03_DrawSectionBeam
                         var top1_Num = (activeSheet.Cells[i, 17] as Range).Value;
                         var top1_Dia = (activeSheet.Cells[i, 18] as Range).Value;
                         BlockTable blockTable = Tx.GetObject(acCurDb.BlockTableId, OpenMode.ForRead) as BlockTable;
-                        ObjectId blockId = blockTable["BeamAll"];
                         BlockTableRecord blkTableRecord = Tx.GetObject(blockTable[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
-                       
+                        
+                        //Chèn khung ký hiệu 
+                        ObjectId blockId = blockTable["BeamAll"];
                         BlockReference blockReference = new BlockReference(new Point3d(point2D.X + (i - firstrow) * 1000, point2D.Y, 0),blockId);
+                        blkTableRecord.AppendEntity(blockReference);
+                        Tx.AddNewlyCreatedDBObject(blockReference, true);
+                        
+                        //Vẽ biên dạng dầm
                         Point2d point2D1 = new Point2d(point2D.X + (i-firstrow)*1000 + 500-width/2,point2D.Y+1150-height/2);
-                        //Specify the Polyline 's coordinates
-                        
-                        Polyline p1 = new Polyline();
-                        p1.AddVertexAt(0, new Point2d(point2D1.X, point2D1.Y), 0, 0, 0);
-                        p1.AddVertexAt(1, new Point2d(point2D1.X + width, point2D1.Y), 0, 0, 0);
-                        p1.AddVertexAt(2, new Point2d(point2D1.X + width, point2D1.Y+height), 0, 0, 0);
-                        p1.AddVertexAt(3, new Point2d(point2D1.X, point2D1.Y+height), 0, 0, 0);
-                        p1.AddVertexAt(4, new Point2d(point2D1.X, point2D1.Y), 0, 0, 0);
-                        
+                        Polyline p1 = Library.drawRectangle(point2D1, width,height);
                         p1.SetDatabaseDefaults();
                         blkTableRecord.AppendEntity(p1);
-                        blkTableRecord.AppendEntity(blockReference);
                         Tx.AddNewlyCreatedDBObject(p1, true);
                         p1.Closed = true;
-                        Tx.AddNewlyCreatedDBObject(blockReference, true);
+                        p1.Layer = "S-Border";
+                        p1.ColorIndex = 3;
+                        
+                        //Vẽ thép đai
+                        Polyline stirrup = Library.drawstirrup(point2D1, width, height, fillet, cover);
+                        stirrup.SetDatabaseDefaults();
+                        stirrup.Closed = true;
+                        stirrup.Layer = "S-Stif";
+                        stirrup.ColorIndex = 171;
+                        blkTableRecord.AppendEntity(stirrup);
+                        Tx.AddNewlyCreatedDBObject(stirrup, true);
+
+                        Point3d pointDim1 = new Point3d(point2D1.X, point2D1.Y + height, 0);
+                        // Vẽ Kích thước ngang
+                        Point3d pointDimWidth = new Point3d(point2D1.X + width, point2D1.Y + height, 0);
+                        Point3d pointDimWidthlocation = new Point3d(point2D1.X + width, point2D1.Y +height+ 100, 0);
+                        RotatedDimension widthdim = new RotatedDimension(0,pointDim1, pointDimWidth, pointDimWidthlocation,"", acCurDb.Dimstyle);
+                        widthdim.TransformBy(ed.CurrentUserCoordinateSystem);
+                        blkTableRecord.AppendEntity(widthdim);
+                        Tx.AddNewlyCreatedDBObject(widthdim, true);
+
+                        // Vẽ Kích thước dọc
+                        Point3d pointDimHeight = new Point3d(point2D1.X, point2D1.Y, 0);
+                        Point3d pointDimHeightlocation = new Point3d(point2D1.X -100, point2D1.Y , 0);
+                        RotatedDimension heightdim = new RotatedDimension(Math.PI/2, pointDim1, pointDimHeight, pointDimHeightlocation, "", acCurDb.Dimstyle);
+                        heightdim.TransformBy(ed.CurrentUserCoordinateSystem);
+                        blkTableRecord.AppendEntity(heightdim);
+                        Tx.AddNewlyCreatedDBObject(heightdim, true);
+
+                        // Lấy block thép chủ
+                        ObjectId tiebarId = blockTable["TieBar"];
+
+                        // Vẽ thép chủ lớp 1
+                        double fullLayer1 = width - 2 * (cover + fillet);
+                        double disLayer1 = fullLayer1 / (top1_Num-1);
+                        for( int j = 1; j< top1_Num + 1;)
+                        {
+                            double tieBarInsY = 0;
+                            if (localtion.Contains("GỐI") ||localtion.Contains("END") == true)
+                            { tieBarInsY = point2D1.Y + height - cover - fillet;}
+                            else if (localtion.Contains("NHỊP") || localtion.Contains("SPAN") == true)
+                            { tieBarInsY = point2D1.Y + cover + fillet; }    
+                            BlockReference layer1Bar = new BlockReference(new Point3d(point2D1.X + cover + fillet + disLayer1*(j-1), tieBarInsY , 0), tiebarId);
+                            blkTableRecord.AppendEntity(layer1Bar);
+                            Tx.AddNewlyCreatedDBObject(layer1Bar, true);
+                            j++;
+                        }    
+
+
                     }
                     Tx.Commit();
                 }
---
+++ 03_DrawSectionBeam/Library.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+
+namespace _03_DrawSectionBeam
+{
+    class Library
+    {
+        public static Polyline drawRectangle (Point2d insertPoint, double width, double height)
+        {
+            Polyline rectangle = new Polyline();
+            rectangle.AddVertexAt(0, new Point2d(insertPoint.X, insertPoint.Y), 0, 0, 0);
+            rectangle.AddVertexAt(1, new Point2d(insertPoint.X + width, insertPoint.Y), 0, 0, 0);
+            rectangle.AddVertexAt(2, new Point2d(insertPoint.X + width, insertPoint.Y + height), 0, 0, 0);
+            rectangle.AddVertexAt(3, new Point2d(insertPoint.X, insertPoint.Y + height), 0, 0, 0);
+            rectangle.AddVertexAt(4, new Point2d(insertPoint.X, insertPoint.Y), 0, 0, 0);
+            return rectangle;
+        }
+        public static Polyline drawstirrup(Point2d insertPoint  , double width, double height, double radius, double offset)
+        {
+            Polyline stirrup = new Polyline();
+            double bugle = Math.Tan(Math.PI / 8);
+            stirrup.AddVertexAt(0, new Point2d(insertPoint.X + offset + radius, insertPoint.Y + offset), 0, 0, 0);
+            stirrup.AddVertexAt(0, new Point2d(insertPoint.X + width - offset - radius, insertPoint.Y + offset), 0, 0, 0);
+            stirrup.AddVertexAt(0, new Point2d(insertPoint.X + width - offset, insertPoint.Y + radius + offset), -bugle, 0, 0);
+            stirrup.AddVertexAt(0, new Point2d(insertPoint.X + width - offset, insertPoint.Y + height-radius - offset), 0, 0, 0);
+            stirrup.AddVertexAt(0, new Point2d(insertPoint.X + width - radius - offset, insertPoint.Y + height - offset ), -bugle, 0, 0);
+            stirrup.AddVertexAt(0, new Point2d(insertPoint.X + radius + offset, insertPoint.Y + height - offset ), 0, 0, 0);
+            stirrup.AddVertexAt(0, new Point2d(insertPoint.X + offset , insertPoint.Y + height - radius - offset), -bugle, 0, 0);
+            stirrup.AddVertexAt(0, new Point2d(insertPoint.X + offset, insertPoint.Y + radius + offset), 0, 0, 0);
+            stirrup.AddVertexAt(0, new Point2d(insertPoint.X + radius + offset, insertPoint.Y + offset), -bugle, 0, 0);
+            return stirrup;
+        }
+    }
+}
