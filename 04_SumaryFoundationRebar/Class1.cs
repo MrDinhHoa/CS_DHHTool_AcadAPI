a1fb5958b3e28aa555be187317c2ae846ee592bc
MrDinhHoa <dinhhoanghoa@gmail.com>
2024-03-21 17:01:37 +0700

    Update

---
+++ 04_SumaryFoundationRebar/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace _04_SumaryFoundationRebar
+{
+    public class Class1
+    {
+    }
+}
---
+++ 04_SummaryFootingRebar/SummaryFootingRebar.cs
@@ -0,0 +1,41 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using System;
+using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Interop.Excel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application = Autodesk.AutoCAD.ApplicationServices.Application;
+using Autodesk.AutoCAD.Geometry;
+using System.Windows;
+
+namespace _04_SummaryFootingRebar
+{
+    public class SummaryFootingRebar
+    {
+        [CommandMethod("TKThepMong")]
+        [Obsolete]
+
+        public static void ThongKeThep()
+        {
+            #region Get the current document and database
+            Document acDoc = Application.DocumentManager.MdiActiveDocument;
+            Database acCurDb = acDoc.Database;
+            Editor ed = acDoc.Editor;
+            #endregion
+
+            #region Get Input from user
+            PromptStringOptions pStFootSize = new PromptStringOptions("\nKích thước móng (BxHxW):");
+            PromptResult pStFootSizeResult = ed.GetString(pStFootSize);
+            string footSizeStr = pStFootSizeResult.StringResult;
+            footSizeStr.
+            MessageBox.Show(pStFootSizeResult.ToString());
+            #endregion
+
+        }
+    }
+}
